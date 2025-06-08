9f3aaa74466b74155f849dc370d4009125c85160
moashraf86 <botoashraf123@gmail.com>
2025-06-08 21:01:15 +0300

    feat: remove unused actionOptions from Cloudinary upload configuration

--- config/plugins.ts
+++ config/plugins.ts
@@ -10,13 +10,6 @@ module.exports = ({ env }) => ({
         api_key: env("CLOUDINARY_KEY"),
         api_secret: env("CLOUDINARY_SECRET"),
       },
-
-      actionOptions: {
-        upload: {
-          folder: "focal",
-        },
-        delete: {},
-      },
     },
   },
   // ...
